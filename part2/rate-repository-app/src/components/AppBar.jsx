3085dd2e5f299366ea1f9cc9d5970f262cc00ede
tauheedbuttt <tauheedbutt@gmail.com>
2025-10-04 17:49:50 +0300

    Exercises 10.6-10.7

--- part2/rate-repository-app/src/components/AppBar.jsx
+++ part2/rate-repository-app/src/components/AppBar.jsx
@@ -1,18 +1,24 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import Constants from "expo-constants";
 import Text from "./Text";
-import { Pressable } from "react-native";
 import theme from "../theme";
+import { Link } from "react-router-native";
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.appBar,
-    padding: 15,
-    paddingTop: 50,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
   },
   pressable: {
-    width: "100%",
+    flex: 1,
+    padding: 15,
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     color: "white",
@@ -21,20 +27,28 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
-  const onPress = () => {
-    console.log("Pressed!");
-  };
   return (
     <View style={styles.container}>
-      <Pressable onPress={onPress} style={styles.pressable}>
-        <Text
-          fontSize={theme.fontSizes.subheading}
-          style={styles.text}
-          fontWeight="bold"
-        >
-          Repositories
-        </Text>
-      </Pressable>
+      <ScrollView horizontal>
+        <Link to="/" style={styles.pressable}>
+          <Text
+            fontSize={theme.fontSizes.subheading}
+            style={styles.text}
+            fontWeight="bold"
+          >
+            Repositories
+          </Text>
+        </Link>
+        <Link to="/sign-in" style={styles.pressable}>
+          <Text
+            fontSize={theme.fontSizes.subheading}
+            style={styles.text}
+            fontWeight="bold"
+          >
+            Sign In
+          </Text>
+        </Link>
+      </ScrollView>
     </View>
   );
 };
--- part2/rate-repository-app/src/components/Main.jsx
+++ part2/rate-repository-app/src/components/Main.jsx
@@ -1,7 +1,9 @@
-import { Text, StyleSheet, View } from "react-native";
-import RepositoryList from "./RepositoryList";
-import AppBar from "./AppBar";
+import { StyleSheet, View } from "react-native";
 import theme from "../theme";
+import AppBar from "./AppBar";
+import RepositoryList from "./RepositoryList";
+import { Navigate, Route, Routes } from "react-router-native";
+import SignIn from "./SignIn";
 
 const styles = StyleSheet.create({
   container: {
@@ -15,7 +17,11 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   );
 };
